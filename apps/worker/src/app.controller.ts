425cfe4620e2cbed26315ffdb598c8c8d3c44f79
Alvaro <agilsansinena@gmail.com>
2026-03-20 13:54:29 +0100

    feat: add worker microservice with RabbitMQ event handling, Dockerfile, and Kubernetes deployment.

--- apps/worker/src/app.controller.ts
+++ apps/worker/src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
+import { EventPattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @EventPattern('task_created') // El nombre del evento que Hugo mandará
+  handleTaskCreated(@Payload() data: any) {
+    console.log('--- NUEVA TAREA RECIBIDA ---');
+    console.log(`Target a escanear: ${data.target}`);
+    console.log(`Tipo de escaneo: ${data.type}`);
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+    // Aquí es donde en el siguiente paso llamaremos a Nmap o Nikto
+    this.executeScan(data.target);
+  }
+
+  private executeScan(target: string) {
+    console.log(`Simulando escaneo de vulnerabilidades en ${target}...`);
+    // Lógica de hacking próximamente...
   }
 }
--- apps/worker/src/main.ts
+++ apps/worker/src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Main');
+  const rmqUrl = process.env.RABBITMQ_URL;
+  const rmqQueue = process.env.RABBITMQ_QUEUE;
+
+  if (!rmqUrl || !rmqQueue) {
+    logger.error('RABBITMQ_URL o RABBITMQ_QUEUE no están definidos');
+    process.exit(1);
+  }
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [rmqUrl],
+        queue: rmqQueue,
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+  await app.listen();
+  logger.log(
+    `Worker de Vulnix listo para recibir órdenes en la cola: ${rmqQueue} 🚀`,
+  );
 }
 bootstrap();

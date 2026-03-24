083818dcfb842df5689b46186772348c250fbdba
Hugo Ceniceros Olave <hcenicerosolave@gmail.com>
2026-03-24 11:35:58 +0100

    feat: Create new `api-core` NestJS application with RabbitMQ microservice integration and Docker setup.

--- apps/api-core/src/app.module.ts
+++ apps/api-core/src/app.module.ts
@@ -1,18 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     // Registramos el cliente de RabbitMQ
     ClientsModule.register([
       {
         name: 'SCAN_SERVICE',
         transport: Transport.RMQ,
         options: {
-          // IP de Tailscale del VPS Master (donde vive Rabbit)
-          urls: [process.env.RABBITMQ_URL!],
+          urls: [process.env.RABBITMQ_URL as string],
           queue: 'scan_tasks',
           queueOptions: { durable: true },
         },
